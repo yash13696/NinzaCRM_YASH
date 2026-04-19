656fbdaf442bb36cb3977f5f474893c3fc0f3775
Yash Kumar Srivatava <yash13696@gmail.com>
2026-04-19 09:55:29 +0530

    Base class update

--- testNinzaCRM/src/main/java/baseClass/BaseClass.java
+++ testNinzaCRM/src/main/java/baseClass/BaseClass.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
@@ -65,6 +66,7 @@ public class BaseClass {
 		driver.findElement(By.xpath("//div[@class='dropdown-item logout']")).click();
 	}
 	
+	@AfterSuite
 	public void afterSuit() {
 		driver.quit();
 	}
